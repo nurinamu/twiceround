ac2fb68c93b4f496c67cd55ffb7602cfc11753de
nurinamu <nurinamu@gmail.com>
2017-02-10 20:12:50 +0900

    저장된 이미지 확인 기능

--- js/main.js
+++ js/main.js
@@ -1,21 +1,60 @@
 //import $ from 'jquery';
 var $ = require('jquery');
 window.$ = window.jquery = $;
+init();
 
-$("#imgBtn").click(main);
-$("#clearBtn").click(cse);
-$("#appendBtn").click(addMoreImage);
+function init() {
+    $("#imgBtn").click(main);
+    $("#clearBtn").click(cse);
+    $("#appendBtn").click(addMoreImage);
+    $("#addApiBtn").click(addApi);
+    $("#listBtn").click(showStored);
+}
+
+var offset = 0;
 
 function main() {
     console.log("init twiceround");
+    chrome.storage.sync.get("twice_meta", function(data){
+        if (data) {
+            if (data.twice_meta && data.twice_meta.apiKey) {
+                start(data.twice_meta.apiKey);
+            } else {
+                openApiInput();
+            }
+        } else {
+            console.log("data is not existed!");
+        }
+    });
+
+}
+
+function openApiInput() {
+    $("#settings").css("display","block");
+}
+
+function addApi() {
+    var newKey = $.trim($("#cseApi").val());
+    if (newKey != '') {
+        chrome.storage.sync.set({
+            "twice_meta" : {
+                apiKey : newKey
+            }
+        }, function() {
+           start(newKey);
+        });
+    }
+}
+
+function start(apiKey) {
     chrome.storage.sync.get("twice_items", function(data){
         console.log(data);
         if (data && data.twice_items.length > 0) {
             console.log("get from cache!");
-            setBackground(data.twice_items);
+            setBackgroundWithArr(data.twice_items);
         } else {
             console.log("get from search results!");
-            cse();
+            cse(apiKey);
         }
     });
 }
@@ -24,17 +63,17 @@ function main() {
 /**
  * google custom search engine을 사용
  */
-function cse(prevLinks) {
+function cse(apiKey) {
     $.ajax({
         url : 'https://www.googleapis.com/customsearch/v1?parameters',
         type : 'GET',
         data : {
-            key : '',
-            cx : '',
+            key : apiKey,
+            cx : '014447932292512077827:p1xwixchex4',
             searchType : 'image',
             imgSize : 'xxlarge',
             fields : "items(link)",
-            q : '트와이스 나연'
+            q : '나연'
         },
         success : function(resp){
             console.log(resp);
@@ -45,7 +84,7 @@ function cse(prevLinks) {
 
             storeData(links);
 
-            setBackground(links);
+            setBackgroundWithArr(links);
         }
     })
 }
@@ -60,10 +99,37 @@ function storeData(data) {
     });
 }
 
-function setBackground(items) {
+function setBackgroundWithArr(items) {
     $("body").css('background-image','url("'+selectData(items)+'")');
+}
+
+function setBackground(url) {
+    $("body").css('background-image','url("'+url+'")');
 }
 
 function selectData(datas) {
     return datas[Math.floor(Math.random()*datas.length)];
+}
+
+function showStored() {
+    chrome.storage.sync.get("twice_items", function(data){
+        console.log(data);
+        if (data && data.twice_items.length > 0) {
+            console.log("get from cache to list!");
+            imgTable = $("<table></table>");
+            imgRow = $("<tr></tr>");
+            for (key in data.twice_items) {
+                imgCell = $("<td></td>");
+                newImg = $("<img></img>");
+                newImg.attr("src", data.twice_items[key]);
+                newImg.click(function(obj){
+                   setBackground(obj.target.src);
+                });
+                imgCell.append(newImg);
+                imgRow.append(imgCell);
+            }
+            imgTable.append(imgRow);
+            $("#imgList").append(imgTable);
+        }
+    });
 }
