7225e73e6b6ec6a9d49516579ddff61e220c59e3
nurinamu <nurinamu@gmail.com>
2017-02-11 20:01:24 +0900

    landscape / portrait 최적화

--- js/main.js
+++ js/main.js
@@ -8,16 +8,18 @@ function init() {
     $("#imgBtn").click(main);
     $("#clearBtn").click(clearAll);
     $("#addApiBtn").click(addApi);
-    $("#listBtn").click(showStored);
+    $("#listBtn").click(toggleStored);
 
     main();
 }
 
 var twiceApiKey;
+var isLandscape;
 
 function main() {
-    console.log("init twiceround");
-    getFromStorage("twice_cse_api_key", function(data){
+    isLandscape = window.innerWidth > window.innerHeight;
+    console.log("init twiceround->isLandscape[" + isLandscape + "]");
+    getFromStorage("twice_cse_api_key", function (data) {
         if (data) {
             twiceApiKey = data.twice_cse_api_key;
             if (twiceApiKey) {
@@ -33,7 +35,7 @@ function main() {
 }
 
 function openApiInput() {
-    $("#settings").css("display","block");
+    $("#settings").css("display", "block");
 }
 
 function addApi() {
@@ -41,23 +43,31 @@ function addApi() {
     if (newKey != '') {
         twiceApiKey = newKey;
         setToStorage({
-            "twice_cse_api_key" : newKey
-        }, function() {
-           start();
+            "twice_cse_api_key": newKey
+        }, function () {
+            start();
         });
     }
 }
 
 function start(apiKey) {
-    getOffset(function(offset) {
-        getFromStorage("twice_items", function(data){
+    getOffset(function (offset) {
+        getFromStorage("twice_items", function (data) {
             console.log(data);
-            if (data && data.twice_items.length >= offset) {
-                console.log("get from cache! ->"+offset);
-                setBackground(data.twice_items[offset-1]);
-                setOffset(offset+1, function(){});
+            if (data.twice_items && data.twice_items.length >= offset) {
+                console.log("get from cache! ->" + offset);
+                if (isLandscape == data.twice_items[offset - 1].isLandscape) {
+                    setBackground(data.twice_items[offset - 1].url);
+                    setOffset(offset + 1, function () {
+                    });
+                } else {
+                    setOffset(offset + 1, function () {
+                    });
+                    console.log("not matched orientation. go next");
+                    start(apiKey);
+                }
             } else {
-                console.log("get search results ->"+offset);
+                console.log("get search results ->" + offset);
                 cse(apiKey, offset);
             }
         });
@@ -70,35 +80,39 @@ function start(apiKey) {
  */
 function cse(apiKey, offset) {
     $.ajax({
-        url : 'https://www.googleapis.com/customsearch/v1?parameters',
-        type : 'GET',
-        data : {
-            key : apiKey,
-            cx : '017778292064153678241:lc5ig_122zu',
-            searchType : 'image',
-            imgSize : 'xxlarge',
-            fields : "items(link)",
-            q : 'twice',
-            start : offset
+        url: 'https://www.googleapis.com/customsearch/v1?parameters',
+        type: 'GET',
+        data: {
+            key: apiKey,
+            cx: '017778292064153678241:lc5ig_122zu',
+            searchType: 'image',
+            imgSize: 'xxlarge',
+            q: 'twice',
+            start: offset
         },
-        success : function(resp){
+        success: function (resp) {
             console.log(resp);
             if (resp.items) {
-                getFromStorage("twice_items", function(data) {
+                getFromStorage("twice_items", function (data) {
                     var twiceItems = data.twice_items;
                     if (twiceItems == undefined) {
                         twiceItems = [];
                     }
-                    for(key in resp.items) {
-                        twiceItems.push(resp.items[key].link);
+                    for (key in resp.items) {
+                        twiceItems.push({
+                            url: resp.items[key].link,
+                            isLandscape: resp.items[key].image.width > resp.items[key].image.height,
+                            thumbnail : resp.items[key].image.thumbnailLink
+                        });
                     }
-                    storeItems(twiceItems, function() {
+                    storeItems(twiceItems, function () {
                         start(apiKey);
+                        showImgList();
                     });
                 });
             } else {
                 if (offset) {
-                    setOffset(1, function(){
+                    setOffset(1, function () {
                         start(apiKey);
                     });
                 } else {
@@ -111,15 +125,15 @@ function cse(apiKey, offset) {
 }
 
 function storeItems(data, callback) {
-    setToStorage({twice_items:data}, callback);
+    setToStorage({twice_items: data}, callback);
 }
 
 function setBackgroundWithArr(items) {
-    $("body").css('background-image','url("'+selectData(items)+'")');
+    $("body").css('background-image', 'url("' + selectData(items) + '")');
 }
 
 function getOffset(callback) {
-    getFromStorage("twice_offset", function(data){
+    getFromStorage("twice_offset", function (data) {
         if (!isNaN(data.twice_offset)) {
             callback(data.twice_offset);
         } else {
@@ -132,51 +146,63 @@ function setOffset(offset, callback) {
     if (offset > maxOffset) {
         offset = 1;
     }
-    setToStorage({twice_offset:offset}, callback);
+    setToStorage({twice_offset: offset}, callback);
 }
 
 function getFromStorage(key, callback) {
-    chrome.storage.sync.get(key, callback);
+    chrome.storage.local.get(key, callback);
 }
 
 function setToStorage(data, callback) {
-    chrome.storage.sync.set(data, callback);
+    chrome.storage.local.set(data, callback);
 }
 
 function setBackground(url) {
-    $("body").css('background-image','url("'+url+'")');
+    $("body").css('background-image', 'url("' + url + '")');
 }
 
 function selectData(datas) {
-    return datas[Math.floor(Math.random()*datas.length)];
+    return datas[Math.floor(Math.random() * datas.length)];
 }
 
-function showStored() {
-    getFromStorage("twice_items", function(data){
+function toggleStored() {
+    if ($("#imgList").css("display") == "none") {
+        showImgList();
+        $("#imgList").css("display", "block");
+    } else {
+        $("#imgList").css("display", "none");
+    }
+
+}
+
+function showImgList() {
+    getFromStorage("twice_items", function (data) {
         console.log(data);
-        if (data && data.twice_items.length > 0) {
+        if (data.twice_items && data.twice_items.length > 0) {
             console.log("get from cache to list!");
-            imgTable = $("<table></table>");
+            imgTable = $("#imgList table");
+            if (imgTable[0].rows.length > 0) {
+                imgTable[0].deleteRow(0);
+            }
             imgRow = $("<tr></tr>");
             for (key in data.twice_items) {
                 imgCell = $("<td></td>");
                 newImg = $("<img></img>");
-                newImg.attr("src", data.twice_items[key]);
-                newImg.click(function(obj){
-                   setBackground(obj.target.src);
+                newImg.attr("src", data.twice_items[key].thumbnail);
+                newImg.click(function (obj) {
+                    setBackground(data.twice_items[key].url);
                 });
                 imgCell.append(newImg);
                 imgRow.append(imgCell);
             }
             imgTable.append(imgRow);
-            $("#imgList").append(imgTable);
         }
     });
 }
 
 function clearAll() {
-    storeItems([], function(){
-        setOffset(1, function(){
+    storeItems([], function () {
+        setOffset(1, function () {
             console.log("clear all data");
         });
     });
