7d4712a66a772ed09c2f34f1025fd9082769e197
nurinamu <nurinamu@gmail.com>
2017-03-09 16:48:34 +0900

    JQuery ts 추가

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,14 +1,15 @@
 var webpack = require("webpack");
 
 module.exports = [{
-    entry : './output/twiceround.js',
+    entry : ['./output/twiceround.js','./js/main.js'],
     output : {
         filename: 'bundle.js',
         path: 'app/js'
     },
     plugins : [
         new webpack.ProvidePlugin({
-            kotlin : './lib/kotlin.js'
+            kotlin : './lib/kotlin.js',
+            $ : 'jquery'
         })
     ]
 }];
--- src/com/nurinamu/kotlin/js/Main.kt
+++ src/com/nurinamu/kotlin/js/Main.kt
@@ -1,9 +1,17 @@
 package com.nurinamu.kotlin.js
 
+import ext.JQuery
+
 /**
  * Created by nurinamu on 2017. 3. 5..
  */
 
+@JsName("$")
+external fun jQuery(selector: String): JQuery
+
+
 fun main(argv: Array<String>) {
     println("new package kotlin.")
+
+    println(jQuery("span").text())
 }
